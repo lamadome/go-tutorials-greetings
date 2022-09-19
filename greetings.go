feb9deb76396997a97849b38d916ebbdfdb8d8e5
Jona Apelbaum <jona@hashicorp.com>
2022-09-19 10:27:03 +0200

    add random

--- greetings.go
+++ greetings.go
@@ -3,16 +3,31 @@ package greetings
 import (
     "fmt"
     "errors"
+    "math/rand"
+    "time"
 )
 
-// Hello returns a greeting for the named person.
+func init() {
+    rand.Seed(time.Now().UnixNano())
+}
+
 func Hello(name string) (string, error) {
-    // If no name was given, return an error with a message.
+    
     if name == "" {
         return "", errors.New("empty name")
     }
-    // Return a greeting that embeds the name in a message.
+    
     message := fmt.Sprintf("Hi, %v. Welcome!", name)
 
     return message, nil
+}
+
+func randomFormat() string {
+    formats := []string{
+        "Hi, %v. Welcome!",
+        "Great to see you, %v!",
+        "Hail, %v! Well met!",
+    }
+
+    return formats[rand.Intn(len(formats))]
 }
