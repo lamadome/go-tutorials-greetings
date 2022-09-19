f4f6660d4fc3ef7726ddb6f949fb5c090221b443
Jona Apelbaum <jona@hashicorp.com>
2022-09-19 10:57:59 +0200

    add multiple hello

--- greetings.go
+++ greetings.go
@@ -11,6 +11,19 @@ func init() {
     rand.Seed(time.Now().UnixNano())
 }
 
+fun Hellos(names []string) (map([string]string),error) {
+    messages := make(map[string]string)
+
+    for _, name := range names {
+        message, err := Hello(name)
+        if err != nil {
+            return nil, err
+        }
+        messages[name] = message
+    }
+    return messages, nil
+}
+
 func Hello(name string) (string, error) {
     
     if name == "" {
